9a726f5b7cc7de57ac55c27e98b649127d603645
sachin nanvare <nanawparashuram564@gmail.com>
2026-04-11 00:31:55 +0530

    fix: seed all questions and fix user stats update and fiexd bug

--- backend/config/seed.js
+++ backend/config/seed.js
@@ -233,7 +233,7 @@ const questions = [
 
 async function seed() {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/quizmern');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
 
     await Question.deleteMany({});
